1fc24def7971961d1bdde4954069864b82ccb819
unknown <izzziej@gmail.com>
2021-11-19 10:31:19 -0600

    Heroku

--- Book-Searcher/Develop/server/server.js
+++ Book-Searcher/Develop/server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const mongoose = require("mongoose");
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,3 +21,13 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+);
